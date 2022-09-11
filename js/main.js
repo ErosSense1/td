3dac9cc95008509456a6c770b745cedd18c83d4f
tiktokxsx@gmail.com <tiktokxsx@gmail.com>
2022-09-10 18:20:02 -0700

    update

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/**@type {HTMLCanvasElement} */
+
+const canvas = document.getElementById( "canvas" );
+const ctx = canvas.getContext( "2d" );
+canvas.width = 1280;
+canvas.height = 768;
+function img ( src ) { let img = new Image(); img.src = src; return img; }
+let backGround = img( "./assets/towerDefense.png" );
+function play ()
+{
+    requestAnimationFrame( play );
+    ctx.drawImage( backGround, 0, 0, canvas.width, canvas.height )/*background*/
+}
